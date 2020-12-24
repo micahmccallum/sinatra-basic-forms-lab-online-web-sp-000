4944d2e11d38af399333fee3d2a862cb935a8925
miqueas <micahmccallum@gmail.com>
2020-12-24 11:56:26 -0500

    Done.

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,29 +1,10 @@
 class Puppy
+  attr_accessor :name, :breed, :age
 
   def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age 
   end
-
-  def name 
-    @name
-  end
-
-  def name=(name)
-    @name = name
-  end
-
-  def breed 
-    @breed 
-  end
-
-  def age 
-    @age 
-  end
-
-  def age=(months)
-    @age = months
-  end 
-  
+    
 end
